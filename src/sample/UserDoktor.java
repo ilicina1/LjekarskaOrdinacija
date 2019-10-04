956593efc3a2cbb1f9ac86fd5396397eb744a426
ilicina1 <ilhan.licina@outlook.com>
2019-10-04 15:45:27 +0200

    Dodata klasa UserDoktor koja ce mi trebati za login informacije iregistraciju, sada se slike pokazuju kada se pokrene program

---
+++ src/sample/UserDoktor.java
@@ -0,0 +1,60 @@
+package sample;
+
+public class UserDoktor {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String eMail;
+
+    public UserDoktor(String firstName, String lastName, String userName, String password, String eMail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.eMail = eMail;
+    }
+
+    public UserDoktor() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+}
