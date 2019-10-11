221273ac651a5a138ca9ca099acc2c6798ea0930
ilicina1 <ilhan.licina@outlook.com>
2019-10-11 16:05:22 +0200

    Klasa Diagnosis napravljena

---
+++ bazz.db.sql
@@ -0,0 +1,29 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "Dijagnoze" (
+	"id"	INTEGER,
+	"text"	TEXT,
+	"pacijent"	INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("pacijent") REFERENCES "Pacijenti"("medical_record_number")
+);
+CREATE TABLE IF NOT EXISTS "Doktori" (
+	"first_name"	TEXT,
+	"last_name"	TEXT,
+	"user_name"	TEXT,
+	"password"	TEXT,
+	"e_mail"	TEXT,
+	PRIMARY KEY("first_name")
+);
+CREATE TABLE IF NOT EXISTS "Pacijenti" (
+	"medical_record_number"	INTEGER,
+	"full_name"	TEXT,
+	"phone_number"	TEXT,
+	"city"	TEXT,
+	"address"	TEXT,
+	"birth_date"	TEXT
+);
+INSERT INTO "Doktori" VALUES ('Ilhan','Licina','ilicina1','Manijaci1921','ilicina1@outlook.com');
+INSERT INTO "Doktori" VALUES ('Marin','Marinac','mmarinac1','Marinac123','mmarinac1@gmail.com');
+INSERT INTO "Pacijenti" VALUES (123,'Manga Mangafic','062322477','Sarajevo','Aleja Pamuka 21','12-06-99');
+INSERT INTO "Pacijenti" VALUES (43,'Pepa Pig','033156877','Sarajevo','Drinska 50c','22-06-87');
+COMMIT;
