7802b0164b6b914b833549ccd94b3b6169a1e538
ilicina1 <ilhan.licina@outlook.com>
2019-10-14 17:43:58 +0200

    dodana je forma za nalaze tj medicalfindings, kao i controller klasa

--- src/sample/PatientsController.java
+++ src/sample/PatientsController.java
@@ -176,4 +176,32 @@ public class PatientsController {
         });
         timeline.play();
     }
+
+    public void actionMedicalFindings(ActionEvent actionEvent) throws IOException, SQLException {
+        Patients pacijent = tableViewPacijenti.getSelectionModel().getSelectedItem();
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/medicalFindings.fxml"));
+            MedicalFindingsController ctrl = new MedicalFindingsController(pacijent);
+            loader.setController(ctrl);
+            stage.setTitle("Medical Findings");
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        root.translateYProperty().set(-533);
+
+        anchor.getChildren().add(root);
+
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(0.45), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            anchor.getChildren().remove(anchor);
+        });
+        timeline.play();
+    }
 }
