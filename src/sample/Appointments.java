b0da89b69ef3b16cafad316e1ca75c59fa0f1f97
ilicina1 <ilhan.licina@outlook.com>
2019-10-17 23:56:16 +0200

    dodavanje forme i klase appointments

---
+++ src/sample/Appointments.java
@@ -0,0 +1,62 @@
+package sample;
+
+import java.time.LocalDate;
+
+public class Appointments {
+    public int id;
+    public String nameAndSurname;
+    public String time;
+    public String reason;
+    public LocalDate date;
+
+    public Appointments() {
+    }
+
+    public Appointments(int id, String nameAndSurname, String time, String reason, LocalDate date) {
+        this.id = id;
+        this.nameAndSurname = nameAndSurname;
+        this.time = time;
+        this.reason = reason;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameAndSurname() {
+        return nameAndSurname;
+    }
+
+    public void setNameAndSurname(String nameAndSurname) {
+        this.nameAndSurname = nameAndSurname;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
