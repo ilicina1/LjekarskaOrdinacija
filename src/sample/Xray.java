2ec0461acac1085a0b148aa1b4df35bd3b609795
ilicina1 <ilhan.licina@outlook.com>
2019-10-18 15:23:12 +0200

    dodata xray klasa

---
+++ src/sample/Xray.java
@@ -0,0 +1,52 @@
+package sample;
+
+import java.time.LocalDate;
+
+public class Xray {
+    private int id;
+    private String whatsOnRay;
+    private LocalDate date;
+    private Patients patient;
+
+    public Xray() {
+    }
+
+    public Xray(int id, String whatsOnRay, LocalDate date, Patients patient) {
+        this.id = id;
+        this.whatsOnRay = whatsOnRay;
+        this.date = date;
+        this.patient = patient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWhatsOnRay() {
+        return whatsOnRay;
+    }
+
+    public void setWhatsOnRay(String whatsOnRay) {
+        this.whatsOnRay = whatsOnRay;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Patients getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patients patient) {
+        this.patient = patient;
+    }
+}
